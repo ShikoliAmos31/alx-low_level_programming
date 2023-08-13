1e3eda37edebffe361973cc6a9cc9930f7325520
shikoliamos31 <amosshikoli@gmail.com>
2023-08-13 04:38:43 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,25 +4,25 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
-* main - main function
+* main - Program that prints alphabets in lowercase and uppercase
 *
-* Return: always 0
+*Return: always 0
 */
 int main(void)
 {
 	int n;
 int m;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 m = n % 10;
 if (m > 5)
-printf("last digit of %d is %d and is greater than 5", n, m);
+printf("the last digit of %d is %d and is greater than 5", n, m);
 else if (m == 0)
-printf("last digit of %d is %d and is zero", n, m);
-if (m < 6 && m ! = 0)
+printf("the last digit of %d is %d and is zero", n, m);
+else if (m < 6 && m != 0)
 printf("last digit of %d is %d and is less than 6 and not zero", n, m);
-printf("\n")
-
+printf("\n");
 	return (0);
 }
