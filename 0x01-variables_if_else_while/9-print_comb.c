c05d20af8b0490b5933619c3d2119de2d35e59ce
shikoliamos31 <amosshikoli@gmail.com>
2023-08-13 15:53:43 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,12 @@ int i;
 
 for (i = 0 ; i < 10 ; i++)
 {
-if (i == 9)
 putchar(i + '0');
-else
-putchar(i + '0');
+if (i != 9)
 putchar('.');
 putchar(' ');
 }
+putchar('\n');
 return (0);
 }
-
-
 
