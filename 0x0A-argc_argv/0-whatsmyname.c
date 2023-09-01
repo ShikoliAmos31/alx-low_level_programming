58d0a1b8b608cc4621e24ec59ed6ea72e08dc114
shikoliamos31 <amosshikoli@gmail.com>
2023-09-01 00:35:13 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main -function that prints a program name
+* @argc: command line argument
+* @argv: argument vector
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# 0x0A. C - argc, argv
+
+i am learning 0x0A. C - argc, argv on the task for this alx projects
