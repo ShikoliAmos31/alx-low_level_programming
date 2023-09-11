b73ddcad2957f17e43341a76df0ff4c1a2721798
shikoliamos31 <amosshikoli@gmail.com>
2023-09-11 07:17:01 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -dog structure
+ * @name: name of dog
+ * @age: name of dog
+ * @owner: name of the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
