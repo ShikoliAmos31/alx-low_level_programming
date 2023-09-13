5336290f90c755bc86f49e194222613abfcc1762
shikoliamos31 <amosshikoli@gmail.com>
2023-09-13 12:04:26 -0700

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,13 @@
 #include "3-calc.h"
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
 /**
-* op_add - Returns the sum of two numbers
+* op_add -function that returns the sum of two numbers
 * @a: The first number
 * @b: The second number
 * Return: The sum of a and b
@@ -15,7 +17,7 @@ int op_add(int a, int b)
 return (a + b);
 }
 /**
-* op_sub - Returns the difference of two numbers
+* op_sub -function that returns the difference of two numbers
 * @a: The first number
 * @b: The second number
 * Return: The difference of a and b
@@ -25,19 +27,17 @@ int op_sub(int a, int b)
 return (a - b);
 }
 /**
-* op_mul - Returns the product of two numbers
+* op_mul -function that returns the product of two numbers
 * @a: The first number
 * @b: The second number
 * Return: The product of a and b
 */
-
 int op_mul(int a, int b)
 {
 return (a * b);
 }
-
 /**
-* op_div - Returns the division of two numbers
+* op_div -function that returns the division of two numbers
 * @a: The first number
 * @b: The second number
 * Return: The quotient of a and b
@@ -47,12 +47,11 @@ int op_div(int a, int b)
 return (a / b);
 }
 /**
-* op_mod - Returns the remainder of the division of two numbers
+* op_mod -function that returns the remainder of the division of two numbers
 * @a: The first number
 * @b: The second number
 * Return: The remainder of the division of a by b
 */
-
 int op_mod(int a, int b)
 {
 return (a % b);
