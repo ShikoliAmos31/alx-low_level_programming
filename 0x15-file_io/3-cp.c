1cdf4fce97fdb51b328a8259da58fd74926c8f8a
ShikoliAmos31 <amosshikoli@gmail.com>
2023-11-02 06:09:42 -0700

    cp file_from file_to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * handle_error -check if the file is open and handle errors
+ * error_file -check if the file is open and handle errors
  * @file_to: file to copy to
  * @file_from: file to copy from and to check
  * @argv: argument vector
  */
-void handle_error(int file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error:Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1)
@@ -38,17 +38,17 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	handle_error(file_from, file_to, argv);
+	error_file(file_from, file_to, argv);
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-			handle_error(-1, 0, argv);
+			error_file(-1, 0, argv);
 		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
-			handle_error(0, -1, argv);
+			error_file(0, -1, argv);
 	}
 
 	err_close = close(file_from);
