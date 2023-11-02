fb776b130ce36d5d37226677767c3ba5995c7482
ShikoliAmos31 <amosshikoli@gmail.com>
2023-11-02 05:07:03 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,19 +7,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int rwr, fd;
-	int letters;
+	int fd;
+	int rwr = 1;
+	int letters = 0;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		text_content = " ";
-	for (letters = 0; text_content[letters]; letters++)
-		;
-	rwr = write(fd, text_content, letters);
+	if (text_content)
+	{
+		while (text_content[letters])
+			letters++;
+		rwr = write(fd, text_content, letters);
+	}
 	if (rwr == -1)
 		return (-1);
 	close(fd);
