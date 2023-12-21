ae799abb9227dac119bb713b71fc6930dd48eb8c
ShikoliAmos31 <amosshikoli@gmail.com>
2023-12-21 11:24:01 -0800

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,54 +1,51 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: the hash table
- * @key: the key
- * @value: the value associated with the key
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: pointer to hash table
+ * @key: key to add the element
+ * @value: value to add the element
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *temp;
-	unsigned long int index;
+	unsigned long int index = 0;
+	char *valuecopy, *keycopy;
+	hash_node_t  *bucket, *new_node;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (!ht || !key || !*key || !value)
+		return (0);
+
+	valuecopy = strdup(value);
+	if (!valuecopy)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	bucket = ht->array[index];
 
-	temp = ht->array[index];
-
-	while (temp != NULL)
+	while (bucket)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (!strcmp(key, bucket->key))
 		{
-			free(temp->value);
-			temp->value = strdup(value);
-
-			if (temp->value == NULL)
-				return (0);
+			free(bucket->value);
+			bucket->value = valuecopy;
 			return (1);
 		}
-		temp = temp->next;
+		bucket = bucket->next;
 	}
-	new_node = malloc(sizeof(hash_node_t));
-
+	new_node = calloc(1, sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-
-	if (new_node->key == NULL || new_node->value == NULL)
 	{
-		free(new_node->key);
-		free(new_node);
+		free(valuecopy);
 		return (0);
 	}
+	keycopy = strdup(key);
+	if (!keycopy)
+		return (0);
+	new_node->key = keycopy;
+	new_node->value = valuecopy;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
