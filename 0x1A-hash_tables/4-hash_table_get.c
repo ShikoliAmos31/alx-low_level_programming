d243405b9b5354e2a2f99df017c380e81cf50321
ShikoliAmos31 <amosshikoli@gmail.com>
2023-12-22 11:42:54 -0800

    >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,26 +1,28 @@
 #include "hash_tables.h"
 /**
- * hash_table_get -function that retrieves a value associated with a key
- * @ht: the hash table you want to look into
- * @key: the key you are looking for
- * Return: the value associated with the element, or NULL if key couldn’t be found
+ * hash_table_get - function to retrieve value associeted with key
+ * @ht: pointer to hash table
+ * @key: key to retrive value
+ *
+ * Return: value or NULL if key does not exit.
  */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	hash_node_t *current;
+	unsigned long int index = 0;
+	hash_node_t  *bucket;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (!ht || !key || !*key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	current = ht->array[index];
+	bucket = ht->array[index];
 
-	while (current != NULL)
+	while (bucket)
 	{
-		if (strcmp(current->key, key) == 0)
-			return (current->value);
-		current = current->next;
+		if (!strcmp(key, bucket->key))
+			return (bucket->value);
+		bucket = bucket->next;
 	}
 	return (NULL);
 }
